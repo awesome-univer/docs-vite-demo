514f7d172b24891c3d79e624ef666437a3f0e346
白熱 <sonne@asaki.me>
2024-06-03 15:31:16 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
 import "./style.css";
-import "@univerjs/design/lib/index.css";
-import "@univerjs/ui/lib/index.css";
-import "@univerjs/docs-ui/lib/index.css";
 
-import { LocaleType, LogLevel, Univer } from "@univerjs/core";
+import { LocaleType, Univer } from "@univerjs/core";
 import { defaultTheme } from "@univerjs/design";
 import { UniverDocsPlugin } from "@univerjs/docs";
 import { UniverDocsUIPlugin } from "@univerjs/docs-ui";
 import { UniverFormulaEnginePlugin } from "@univerjs/engine-formula";
 import { UniverRenderEnginePlugin } from "@univerjs/engine-render";
 import { UniverUIPlugin } from "@univerjs/ui";
+import { zhCN, enUS } from 'univer:locales'
 
 import { DEFAULT_DOCUMENT_DATA_CN } from './data'
 
@@ -17,7 +15,10 @@ import { DEFAULT_DOCUMENT_DATA_CN } from './data'
 const univer = new Univer({
   theme: defaultTheme,
   locale: LocaleType.ZH_CN,
-  logLevel: LogLevel.VERBOSE,
+  locales: {
+    [LocaleType.ZH_CN]: zhCN,
+    [LocaleType.EN_US]: enUS,
+  },
 });
 
 univer.registerPlugin(UniverRenderEnginePlugin);
@@ -25,13 +26,9 @@ univer.registerPlugin(UniverFormulaEnginePlugin);
 
 univer.registerPlugin(UniverUIPlugin, {
   container: 'app',
-  header: true,
-  toolbar: true,
 });
 
-univer.registerPlugin(UniverDocsPlugin, {
-  standalone: true,
-});
+univer.registerPlugin(UniverDocsPlugin);
 univer.registerPlugin(UniverDocsUIPlugin, {
   container: 'univerdoc',
   layout: {
